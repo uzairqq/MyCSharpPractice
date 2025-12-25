8b3c4609731bb57d60acddd2f30bd4b6023d8fca
Uzair Iqbal <uzair.qq@outlook.com>
2025-12-25 20:43:05 +0500

    Day 2: Learned Assignment Operators - Practiced all assignment operators (+=, -=, *=, /=, %=)

--- Day02-Operators/Program.cs
+++ Day02-Operators/Program.cs
@@ -2,33 +2,26 @@
 {
     static void Main()
     {
-       int num1 =10;
-       int num2=4;
+       int x = 10;
+       x += 5;   // x = x + 5  → x becomes 15
+       Console.WriteLine($"Plus {x}");
+       x -= 3;   // x = x - 3  → x becomes 12
+       Console.WriteLine($"Minus {x}");
+       x *= 2;   // x = x * 2  → x becomes 24
+       Console.WriteLine($"Multiply {x}");
+       x /= 4;   // x = x / 4  → x becomes 6
+       Console.WriteLine($"Qoutient {x}");
+       x %= 5;   // x = x % 5  → x becomes 1
+       Console.WriteLine($"Modulus {x}");
+        Console.WriteLine("");
 
-       //Addition
-       int sum=num1+num2;
-       Console.WriteLine($"Sum: {sum}");
-
-       //Subtraction
-       int difference=num1-num2;
-       Console.WriteLine($"Difference {difference}");
-
-       //Multiplication
-       int product=num1*num2;
-       Console.WriteLine($"Product {product}");
-
-       //Division
-       int quotient=num1/num2;
-       Console.WriteLine($"Quotient {quotient}");
-
-       // Decimal division
-       double quotientDecimal = (double)num1 / num2;
-       Console.WriteLine($"Decimal Quotient: {quotientDecimal}");
-
-       //Modulus (remainder)
-       int remainder=num1%num2;
-       Console.WriteLine($"Remainder {remainder}");
-
-
+       int counter=10;
+       Console.WriteLine($"Initial: {counter}");
+       counter+=5;
+       Console.WriteLine($"Add by 5:- {counter}");
+       counter*=2;
+       Console.WriteLine($"Multiply by 2:- {counter}");
+       counter-=3;
+       Console.WriteLine($"Subtract by 3:- {counter}");
     }
 }
