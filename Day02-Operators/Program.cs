e074792032d3d501a5e7bce246e8a227e4e8ee53
Uzair Iqbal <uzair.qq@outlook.com>
2025-12-25 20:29:59 +0500

    Day 2: Learned Arithmetic Operators and Integer vs Decimal Division

---
+++ Day02-Operators/Program.cs
@@ -0,0 +1,34 @@
+﻿class Program
+{
+    static void Main()
+    {
+       int num1 =10;
+       int num2=4;
+
+       //Addition
+       int sum=num1+num2;
+       Console.WriteLine($"Sum: {sum}");
+
+       //Subtraction
+       int difference=num1-num2;
+       Console.WriteLine($"Difference {difference}");
+
+       //Multiplication
+       int product=num1*num2;
+       Console.WriteLine($"Product {product}");
+
+       //Division
+       int quotient=num1/num2;
+       Console.WriteLine($"Quotient {quotient}");
+
+       // Decimal division
+       double quotientDecimal = (double)num1 / num2;
+       Console.WriteLine($"Decimal Quotient: {quotientDecimal}");
+
+       //Modulus (remainder)
+       int remainder=num1%num2;
+       Console.WriteLine($"Remainder {remainder}");
+
+
+    }
+}
