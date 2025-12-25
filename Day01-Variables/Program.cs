f14c69c862f9e9493fb49b5e40661b4d7b0d77fe
Uzair Iqbal <uzair.qq@outlook.com>
2025-12-25 15:43:36 +0500

    Day 1: Learned Variables and Data Types - Created person profile

---
+++ Day01-Variables/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        //Variables for a Person
+        string name="Uzair";
+        int myAge=33;
+        decimal salary=50000.75m;
+        bool isEmployed=true;
+
+        Console.WriteLine($"My Name is: {name}");
+        Console.WriteLine($"My Age is: {myAge}");
+        Console.WriteLine($"My Salary is: {salary}");
+        Console.WriteLine($"Am I Employed?: {isEmployed}");
+    }
+}
